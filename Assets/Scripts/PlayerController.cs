a6688a40bcfa31bcd454391aff9f1f0b895ed6a9
Jbaker8390 <jonathantuan02@gmail.com>
2018-03-01 22:11:32 -0800

    added camera viewport for player movement restriction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 15.0f;
+    public float padding = 1f;
     // Use this for initialization
+    float xmin;
+    float xmax;
     void Start()
     {
+        //distance between camera and player
+        float distance = transform.position.z - Camera.main.transform.position.z;
 
+        Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+
+        xmin = leftMost.x + padding;
+        xmax = rightMost.x - padding;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xmin = -5;
-        float xmax = 5;
+       
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             //transform.position += new Vector3(-speed *Time.deltaTime, 0, 0);
