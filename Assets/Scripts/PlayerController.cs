38cf0eb92f664ba0c21e0c8516621c2bcb99f907
Jbaker8390 <jonathantuan02@gmail.com>
2018-03-01 21:00:58 -0800

    restricting player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,6 +14,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float xmin = -5;
+        float xmax = 5;
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             //transform.position += new Vector3(-speed *Time.deltaTime, 0, 0);
@@ -23,8 +25,14 @@ public class PlayerController : MonoBehaviour
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            transform.position += Vector3.right * speed * Time.deltaTime;
         }
+
+        //will be the transform potion of the X value of the transform but clamped between the min and max
+        float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+        //restrict player to the gamespace
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+
 
     }
 
